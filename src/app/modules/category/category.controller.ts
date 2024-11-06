a0f3924a7b3e83d1cbd902fc41449d5feb1167be
Khaled Ahmed Nayeem <khaledahmednayeem2004@gmail.com>
2024-11-06 11:59:31 +0600

    complet the category work

--- src/app/modules/category/category.controller.ts
+++ src/app/modules/category/category.controller.ts
@@ -1,8 +1,13 @@
 import * as factory from '../../utils/handlerFactory';
 import Category from './category.model';
 
+// Create a new category using the factory's createOne function
 export const createCategory = factory.createOne(Category);
+// Get a single category by its ID using the factory's getOne function
 export const getCategory = factory.getOne(Category);
+// Get all categories using the factory's getAll function
 export const getAllCategories = factory.getAll(Category);
+// Update a category by its ID using the factory's updateOne function
 export const updateCategory = factory.updateOne(Category);
+// Delete a category by its ID using the factory's deleteOne function
 export const deleteCategory = factory.deleteOne(Category);
